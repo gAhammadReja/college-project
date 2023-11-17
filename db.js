280e1a0286de2e8510490765364b44fcf0e62dd6
Golam Ahammad Reja <141209233+gAhammadReja@users.noreply.github.com>
2023-11-17 22:10:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const Connection = async(username, password) =>{
-    // const URL = `mongodb+srv://${username}:${password}@atlascluster.wpxv39e.mongodb.net/test?retryWrites=true&w=majority&appName=AtlasApp
-    // `;
-      const URL = `mongodb://127.0.0.1:27017/colleges`
+    const URL = `mongodb+srv://${username}:${password}@atlascluster.wpxv39e.mongodb.net/colleges?retryWrites=true&w=majority&appName=AtlasApp
+    `;
+    //   const URL = `mongodb://127.0.0.1:27017/colleges`
         await mongoose.connect(URL, {useUnifiedTopology:true, useNewUrlParser:true, });
         console.log('Database Connect Successfully');
     try{
