c3b85f21e8d7bd5eb18591e77bf4764619b397e5
Golam Ahammad Reja <141209233+gAhammadReja@users.noreply.github.com>
2023-12-03 23:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { getFilterColleges, collgesForAdminPanel , deleteCollege} from './controller.js'
-
+import nodemailer from 'nodemailer';
 // import nodemailer from 'nodemailer'
 dotenv.config();
 const dbusername = process.env.DB_USERNAME;
 const dbpassword = process.env.DB_PASSWORD;
-const baseUrl = process.env.BASE_URL;
-
-Connection(dbusername,dbpassword);
-
-
+// const baseUrl = process.env.BASE_URL;
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+Connection(dbusername,dbpassword);
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -47,12 +49,55 @@ app.get('/login',(req,res)=>{
 
 
 
+//nodemailer
+const emailPass = process.env.EMAILPASS;
+
+const contactEmail = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+      user:'bwebsite865@gmail.com',
+      pass:emailPass,
+    }
+  })
+  contactEmail.verify((error)=>{
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Ready To Send');
+    }
+  })
+
+  
+app.post('/contact',(req,res)=>{
+    const cname = req.body.cname;
+    const cemail = req.body.cemail;
+    const cphone = req.body.cphone;
+    const cmsg = req.body.cmsg;
+
+
+    const TheMail = {
+        from: cname,
+        to:'bwebsite865@gmail.com',
+        subject:'MCET USER CONTACT',
+        html:`
+        <h3>NAME : ${cname}</h3>
+        <h3>EMAIL : ${cemail}</h3>
+        <h3>Phone Number : ${cphone}</h3>
+        <h3>MESSAGE : ${cmsg}</h3>
+        `
+    }
+    contactEmail.sendMail(TheMail,(error)=>{
+        if (error) {
+            res.json({status:'Sorry We Are Unable To Send Your Message Now !'})
+        } else {
+            res.json({status:'Message Sent ! We Will Response Soon'})
+        }
+    });
+  
+  });
 
 
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json())
